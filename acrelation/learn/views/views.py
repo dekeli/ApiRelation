8805a9f488509c2062b178fbe6daad569ac3c66d
deke <liwenzheng3701@163.com>
2018-03-09 21:40:21 +0800

    增加查询代码

--- acrelation/learn/views/views.py
+++ acrelation/learn/views/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from ..models import ApiMessage
 from django.core.paginator import Paginator
+from django.core import serializers
+from django.core.exceptions import ValidationError
 import json
 
 # Create your views here.
@@ -25,29 +27,17 @@ def get_kwarg(data={}):
     return kwargs
 
 def search_api_message(request):
-    response = HttpResponse()
-    data = dict()
-    data["result"] = "fail"
     apidata = {}
-    if request.method == "POST":
-        try:
-            search_data = {
-                "component_owner": request.POST.get("component_owner"),
-                "request_name": request.POST.get("request_name"),
-                "url_name": request.POST.get("url_name")
-            }
-            kwargs = get_kwarg(search_data)
-            apis = ApiMessage.objects.filter(**kwargs)
-            current_page = request.GET.get("page", 1)
-            pages = Paginator(apis, 10)
-            apis = pages.page(current_page)
-            apidata["apis"] = apis
-            apidata["pages"] = pages
-            data['result'] = 'success'
-        except Exception as e:
-            data["message"] = str(e)
-            data["result"] = "fail"
-            response = HttpResponse(status=500)
-        data_json = json.dumps(data)
-        response.write(data_json)
-        return response
+    search_data = {
+        "component_owner": request.GET.get("component_owner"),
+        "request_name": request.GET.get("request_name"),
+        "url_name": request.GET.get("url_name")
+    }
+    kwargs = get_kwarg(search_data)
+    apis = ApiMessage.objects.filter(**kwargs)
+    current_page = request.GET.get("page", 1)
+    pages = Paginator(apis, 10)
+    apis = pages.page(current_page)
+    apidata["apis"] = apis
+    apidata["pages"] = pages
+    return render(request, 'api_message.html', apidata)
